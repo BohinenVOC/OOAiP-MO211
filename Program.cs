e68302b44541cf0141ed3c842e96b5198b968b66
Даниил Божко <bozhkod37@gmail.com>
2022-09-22 14:43:35 +0600

    Inition commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaceBattle;
+
+public interface IMovemable
+{
+    protected static int[] position;
+    protected static int[] velocity;
+    protected static bool movable = true;
+}
+
+
+class SpaceBattle
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine(100);
+        Console.ReadKey();
+        return 0;
+    }
+}
