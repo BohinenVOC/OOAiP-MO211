87882ad5cb4a47a44d2bc65a76d9081c1d3f3ac4
Kirill Iost <84028357+FoxyT0@users.noreply.github.com>
2023-01-12 18:28:51 +0600

    Update LongCommandUnitTest.cs

--- SpaceBattle.Lib.Test/LongCommandUnitTest.cs
+++ SpaceBattle.Lib.Test/LongCommandUnitTest.cs
@@ -3,9 +3,9 @@ using Hwdtech;
 using Hwdtech.Ioc;
 using Moq;
 
-public class LongTermCommandTests
+public class LongCommandTests
 {
-    public LongTermCommandTests()
+    public LongCommandTests()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
@@ -27,7 +27,7 @@ public class LongTermCommandTests
     }
 
     [Fact]
-    public void LongTermOperationStrategyTest()
+    public void LongCommandTest()
     {
         IStrategy LongTermOperation = new LongCommand();
         string name = "Moving";
